e453acede4726e51219b67bc07dcbe4ea0fb3fa6
Aryanftw <ap890383@gmail.com>
2024-10-30 21:54:41 +0530

    created landing page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './main.css'
+import Intropage from './components/Intropage'
+
+const App = () => {
+  return (
+    <>
+    <Intropage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Intropage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Intropage.css'
+const Intropage = () => {
+  return (
+    <div className="main flex flex-col justify-center items-center h-screen font-custom">
+      <h1 className='text-7xl font-bold text-white'>Welcome to my Portfolio Site</h1>
+      <button class="border-2 border-blue-900  w-25 mt-5 text-white py-2 px-4 bg-gray-800 hover:text-gray-300 hover:bg-blue-950 text-white">
+    Know More 
+</button>
+
+    </div>
+  )
+}
+
+export default Intropage
