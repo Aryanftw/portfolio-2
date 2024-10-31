c494585f3c12406bada2d1e4022e924b81c10336
Aryanftw <ap890383@gmail.com>
2024-10-31 10:54:50 +0530

    css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
 import './Home.css'
+import Hero from './Hero'
 
 const Home = () => {
   return (
-    <div className='main min-h-screen'><Navbar/></div>
+    <div className='main min-h-screen'><Navbar/>
+    <Hero/>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div>
-      <div className='flex justify-evenly text-center font-damn'>
+      <div className='flex justify-evenly text-center font-custom'>
         <p className='nav text-3xl font-bold mt-3 mr-10'><a href="">ARYAN</a></p>
         <p className='nav text-2xl font-semibold mt-3'><a href="">Projects</a></p>
         <p className='nav text-2xl font-semibold mt-3 pr-4 mr-3'><a href="">Skills</a></p>
