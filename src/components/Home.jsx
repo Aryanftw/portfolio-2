4ebac35451fa7ec6b8eebe99f9da3a1418232d5e
Aryanftw <ap890383@gmail.com>
2024-10-31 09:40:15 +0530

    added bg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='main min-h-screen'><Navbar/></div>
   )
 }
 
