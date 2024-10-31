482c120eb027d9499501e0ddb2b421f70eb7c7f1
Aryanftw <ap890383@gmail.com>
2024-10-31 09:24:23 +0530

    added routing on the main button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './main.css'
 import Intropage from './components/Intropage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
 
 const App = () => {
   return (
-    <>
-    <Intropage/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Intropage />} />
+        <Route path="/Home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
