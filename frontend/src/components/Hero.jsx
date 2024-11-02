ba0f3e7990f715355e52801cb249b4c8a9cc32f9
Aryanftw <ap890383@gmail.com>
2024-11-02 11:28:44 +0530

    responsive hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import './Hero.css'
+import './Hero.css';
 
 const Hero = () => {
   return (
-    <div id="hero" className="flex min-h-screen flex-col items-center max-w-7xl ">
-      <p className="text-5xl pr-20 mt-56 ml-80 font-bold font-custom animate-fadeInUp">
-        Hello Everyone, <span className="text-white">Aryan</span> this side !
+    <div id="hero" className="flex min-h-screen flex-col items-center max-w-7xl mx-auto text-center px-6">
+      <p className="text-5xl md:text-4xl sm:text-3xl mt-56 font-bold font-custom animate-fadeInUp">
+        Hello Everyone, <span className="text-white">Aryan</span> this side!
       </p>
-      <p className="animate-fadeInUp text-3xl pr-20  ml-80 font-semibold font-custom mt-5">A Passionate Dev Crafting Digital Experiences</p>
-      <p className="text-2xl font-medium  ml-80  font-custom mt-5 animate-fadeInUp font-damn">With expertise in web development and design, I specialize in creating intuitive, user-friendly interfaces that bring ideas to life.</p>
-      <button className="p-1 rounded-3xl animate-fadeInUp font-semibold mt-5 ml-60 text-xl border-2 text-white bg-black w-40 h-14 border-black"><a href="#contact">Call to Action</a></button>
+      <p className="animate-fadeInUp text-3xl md:text-2xl sm:text-xl font-semibold font-custom mt-5">
+        A Passionate Dev Crafting Digital Experiences
+      </p>
+      <p className="text-2xl md:text-xl sm:text-lg font-medium font-custom mt-5 animate-fadeInUp">
+        With expertise in web development and design, I specialize in creating intuitive, user-friendly interfaces that bring ideas to life.
+      </p>
+      <button className="p-1 rounded-3xl animate-fadeInUp font-semibold mt-5 text-xl text-white bg-black w-40 h-14">
+        <a href="#contact">Call to Action</a>
+      </button>
     </div>
   );
 };
