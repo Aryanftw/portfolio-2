33f019d3be16bf2128862c18017bbc02d2b7d34d
Aryanftw <ap890383@gmail.com>
2024-11-02 23:04:00 +0530

    new-repo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Home from './components/Home';
 
 const App = () => {
   return (
-    <BrowserRouter basename='"/portfolio'>
+    <BrowserRouter >
       <Routes>
         <Route path="/" element={<Intropage />} />
         <Route path="/home" element={<Home />} />
